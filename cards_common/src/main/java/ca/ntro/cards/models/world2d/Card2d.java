3a039c7fabf7067f104f5805b3f50ada69100357
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-04-28 09:23:16 -0400

    en cours) levelOfDetails dans l'affichage d'une carte

--- cards_common/src/main/java/ca/ntro/cards/models/world2d/Card2d.java
+++ cards_common/src/main/java/ca/ntro/cards/models/world2d/Card2d.java
@@ -1,6 +1,7 @@
 package ca.ntro.cards.models.world2d;
 
 import ca.ntro.app.views.controls.canvas.GraphicsContextFx;
+import ca.ntro.app.world2d.Object2dDrawingOptions;
 import ca.ntro.cards.models.values.Card;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -29,12 +30,38 @@ public class Card2d extends Object2dCards {
 	}
 
 	@Override
-	public void draw(GraphicsContextFx gc) {
-		gc.getRawGraphicsContext().setFill(Color.CORAL);
-		gc.fillRect(getTopLeftX(), 
-				    getTopLeftY(),
-				    getWidth(), 
-				    getHeight());
+	public void draw(GraphicsContextFx gc, Object2dDrawingOptions options) {
+		
+		// TODO: do something meaningful here ;-)
+		if(levelOfDetails(options) <= 5) {
+
+			gc.strokeRect(getTopLeftX(), 
+						  getTopLeftY(),
+						  getWidth(), 
+						  getHeight());
+			
+		}else {
+
+			gc.getRawGraphicsContext().setFill(Color.CORAL);
+			gc.fillRect(getTopLeftX(), 
+						getTopLeftY(),
+						getWidth(), 
+						getHeight());
+		}
+
+	}
+	
+	private int levelOfDetails(Object2dDrawingOptions options) {
+		int levelOfDetails = 10;
+		
+		if(options.screenWidth(getWidth()) <= 10
+				|| options.screenHeight(getHeight()) <= 50) {
+			
+			levelOfDetails = 4;
+
+		}
+		
+		return levelOfDetails;
 	}
 
 
