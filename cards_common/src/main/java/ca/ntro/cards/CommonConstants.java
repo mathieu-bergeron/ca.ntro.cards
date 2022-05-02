d41fc6984bed5f74730905f15727de0c80314407
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-05-02 08:48:37 -0400

    ajout) constantes pour la taille initiale du monde

---
+++ cards_common/src/main/java/ca/ntro/cards/CommonConstants.java
@@ -0,0 +1,16 @@
+package ca.ntro.cards;
+
+public class CommonConstants {
+	
+	public static final double CARD_WIDTH_MILIMETERS = 50;    // CARD_HEIGHT_MILIMETERS * 2/3
+	public static final double CARD_HEIGHT_MILIMETERS = 75;
+	
+	public static final int INITIAL_WORLD_WIDTH_IN_CARDS = 30;
+	public static final int INITIAL_WORLD_HEIGHT_IN_CARDS = INITIAL_WORLD_WIDTH_IN_CARDS;
+
+	public static final double INITIAL_WORLD_WIDTH = INITIAL_WORLD_WIDTH_IN_CARDS * CARD_WIDTH_MILIMETERS;
+	public static final double INITIAL_WORLD_HEIGHT = INITIAL_WORLD_HEIGHT_IN_CARDS * CARD_HEIGHT_MILIMETERS;   
+	
+	
+
+}
---
+++ cards_playground/src/main/java/ca/ntro/cards/playground/PlaygroundConstants.java
@@ -0,0 +1,10 @@
+package ca.ntro.cards.playground;
+
+import ca.ntro.cards.CommonConstants;
+
+public class PlaygroundConstants {
+	
+	public static final double INITIAL_WORLD_WIDTH = CommonConstants.INITIAL_WORLD_WIDTH;
+	public static final double INITIAL_WORLD_HEIGHT = CommonConstants.INITIAL_WORLD_HEIGHT;
+
+}
