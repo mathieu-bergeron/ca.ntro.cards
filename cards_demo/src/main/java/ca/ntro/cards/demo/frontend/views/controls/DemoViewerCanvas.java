688c0aecaa798835e198218a595f4a727271213f
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-05-05 17:59:41 -0400

    autocommit

--- cards_demo/src/main/java/ca/ntro/cards/demo/frontend/views/controls/DemoViewerCanvas.java
+++ cards_demo/src/main/java/ca/ntro/cards/demo/frontend/views/controls/DemoViewerCanvas.java
@@ -4,15 +4,4 @@ import ca.ntro.cards.frontend.views.controls.ViewerCanvas;
 
 public class DemoViewerCanvas extends ViewerCanvas {
 
-	@Override
-	protected void onInitialSize(double initialWidth, double initialHeight) {
-		super.onInitialSize(initialWidth, initialHeight);
-	}
-
-	@Override
-	protected void onNewSize(double oldWidth, double oldHeight, double newWidth, double newHeight) {
-		super.onNewSize(oldWidth, oldHeight, newWidth, newHeight);
-	}
-
-
 }
--- cards_playground/src/main/java/ca/ntro/cards/playground/models/world2d/World2d.java
+++ cards_playground/src/main/java/ca/ntro/cards/playground/models/world2d/World2d.java
@@ -1,7 +1,5 @@
 package ca.ntro.cards.playground.models.world2d;
 
-
-import ca.ntro.cards.models.enums.Suit;
 import ca.ntro.cards.models.world2d.CommonWorld2d;
 
 public class World2d extends CommonWorld2d {
