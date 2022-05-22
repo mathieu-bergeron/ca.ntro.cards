c591fcdafe5e62058336ee6fadc89115e9b9dbe9
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-05-22 06:16:26 -0400

    en cours) maquette EfficiencyApp

--- demo_efficiency/src/main/java/ca/ntro/cards/demo/frontend/views/DemoEfficiencyDashboardView.java
+++ demo_efficiency/src/main/java/ca/ntro/cards/demo/frontend/views/DemoEfficiencyDashboardView.java
@@ -15,41 +15,13 @@ public class DemoEfficiencyDashboardView extends EfficiencyDashboardView {
 	private Label fpsLabel;
 
 	@FXML
-	private Label numberOfStepsLabel;
-
-	@FXML
-	private Label currentStepLabel;
-
-	@FXML
-	private Label numberOfCardsLabel;
-
-	@FXML
 	private Button menuButton;
-
-	@FXML
-	private Button playButton;
-
-	@FXML
-	private Button pauseButton;
-
-	@FXML
-	private Button oneStepButton;
-
-	@FXML
-	private Button backStepButton;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 		Ntro.assertNotNull("fpsLabel", fpsLabel);
-		Ntro.assertNotNull("numberOfStepsLabel", numberOfStepsLabel);
-		Ntro.assertNotNull("currentStepLabel", currentStepLabel);
-		Ntro.assertNotNull("numberOfCardsLabel", numberOfCardsLabel);
 		Ntro.assertNotNull("menuButton", menuButton);
-		Ntro.assertNotNull("playButton", playButton);
-		Ntro.assertNotNull("pauseButton", pauseButton);
-		Ntro.assertNotNull("oneStepButton", oneStepButton);
-		Ntro.assertNotNull("backStepButton", backStepButton);
 		
 		super.initialize(location, resources);
 		
